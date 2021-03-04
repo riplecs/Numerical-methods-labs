9a37f406c362c4b4280c4288f2678437e065b17b
riplecs <48178483+riplecs@users.noreply.github.com>
2021-03-04 18:43:01 +0200

    Update laba2.py

--- laba2.py
+++ laba2.py
@@ -8,47 +8,72 @@ import numpy as np
 
 def Gauss(AA):
     A=AA
-    print('A = ', AA)
-    strs=[]
-    while len(A)!=1:
+    print(AA, ' = A')
+    B=[]; X=[]
+    x=['x1', 'x2', 'x3', 'x4', 'x5']
+    while len(A)!=0:
         m=A[0][0]
         for i in range(len(A)):
             for j in range(len(A)):
-                if abs(A[i][j])>abs(m):  m=A[i][j]
-        for i in range(len(A)):
-            for j in range(len(A)):
+                if abs(A[i][j])>abs(m): m=A[i][j]
                 if A[i][j]==m: p=i; q=j
+        print(f'\na_main = {m}, p={p}, q={q}\n')
+        for i in range(len(A[0])): A[p][i]=A[p][i]/m
+        print('Нормуємо головний рядок:\n')
+        print(A, ' = A ')
+        v=A[p].copy()
+        A[p]=A[0] 
+        A[0]=v
+        for i in range(len(A)):
+            v=A[i][q].copy()
+            A[i][q]=A[i][0]
+            A[i][0]=v
+        r=x[q]
+        x[q]=x[0]
+        x[0]=r
+        X.append(x)
+        x=x[1:]
         M=len(A)*[0]
         for k in range(len(A)):
-            if k==p: continue
-            M[k]=-A[k][q]/m
+            if k==0: continue
+            M[k]=-A[k][0]/A[0][0]
         for i in range(len(A)):
             for j in range(len(A[0])):
-                if i==p: continue
-                A[i][j]=A[i][j]+A[p][j]*M[i]
-        strs.append(A[p])
-        idx=np.where(A==A[p][q])
+                if i==0: continue
+                A[i][j]=A[i][j]+A[0][j]*M[i]
+        B.insert(0, A[0])
+        print('\nПересуваємо a_main у верхній лівий куток і утворюємо під ним нулі:\n')
+        print(A, ' = A ')
+        idx=np.where(A==A[0][0])
         A=np.delete(A, idx[0][0], axis=0)
         A=np.delete(A, idx[1][0], axis=1)
-        print('A = ', A)
-    strs.append(A[0])
     k=0
-    B=np.zeros((len(AA), len(AA)+1))
-    for i in reversed(strs):
+    A=np.zeros((len(AA), len(AA)+1))
+    for i in reversed(B):
         x=list(i[:(len(i)-1)])
         y=list(i[(len(i)-1):])
-        B[k]=np.array(x+list((len(B[0])-len(i))*[0])+y).reshape(1, -1)
+        A[k]=np.array(list((len(A[0])-len(i))*[0])+x+y).reshape(1, -1)
         k=k+1
-    print('B = ', B)
-    x1=B[0][-1]/B[0][0]
-    x2=(B[1][-1]-x1*B[1][0])/B[1][1]
-    x3=(B[2][-1]-x1*B[2][0]-x2*B[2][1])/B[2][2]
-    x4=(B[3][-1]-x1*B[3][0]-x2*B[3][1]-x3*B[3][2])/B[3][3]
-    print('x1 = ', x1)
-    print('x2 = ', x2)
-    print('x3 = ', x3)
-    print('x4 = ', x4)
-    return A
+    print('\nРезультат:\n')
+    print(A, ' = A')
+    print(X)
+    res=[]
+    n=len(A)
+    for i in reversed(range(n)):
+        result=A[i][-1]
+        k=0
+        for j in reversed((range(n-i))):
+            r=A[i][n-j]
+            if r!=A[i][-1]:
+                result=result-r*res[k]
+                k=k+1
+        res.insert(0, result)
+    B=res[::-1]
+    mas=zip(reversed(X), B)
+    xs=sorted(mas, key=lambda tup: tup[0])
+    B=[b[1] for b in xs]
+    print('res = ', B)
+    return A, B
     
 B=np.array([[1.0, 5.0, 3.0, -4.0, 20.0],
             [3.0, 1.0, -2.0, 0.0, 9.0],
