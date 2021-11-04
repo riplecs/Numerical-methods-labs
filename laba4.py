94c506df7cb65b89d1d6c97ed1219ffd9d5f3833
riplecs <48178483+riplecs@users.noreply.github.com>
2021-11-04 11:09:33 +0200

    Update laba4.py

--- laba4.py
+++ laba4.py
@@ -8,68 +8,68 @@ Created on Sun Mar 28 22:48:24 2021
 import numpy as np
 import math
 
-A=np.array([[7.41, 1.13, 0.93, 1.22],
-            [1.13, 8.31, 1.30, 0.16],
-            [0.93, 1.30, 5.42, 2.10],
-            [1.22, 0.16, 2.10, 11.10]])
+A = np.array([[7.41, 1.13, 0.93, 1.22],
+              [1.13, 8.31, 1.30, 0.16],
+              [0.93, 1.30, 5.42, 2.10],
+              [1.22, 0.16, 2.10, 11.10]])
 
 def max_up_diag(mat):
-    triangle=[]
+    triangle = []
     for i in range(len(mat)):
         for j in range(len(mat)):
-            if i>j:
+            if i > j:
                 triangle.append(mat[i][j])
     return max(triangle, key=abs)
 
 
 def vectors(mat, k):
     return np.matrix([mat[i][k] for i in range(len(mat))])
-eps=0.00001
+eps = 0.00001
 
 def Jakobi(M):
-    v=np.eye(len(M))
-    M0=M.copy()
-    main=max_up_diag(M0)
-    while abs(main)>eps:
+    v = np.eye(len(M))
+    M0 = M.copy()
+    main = max_up_diag(M0)
+    while abs(main) > eps:
         Sd, Snd, SA = 0, 0, 0
         for i in range(len(M0)):
             for j in range(len(M0)):
-                SA=SA+M0[i][j]**2
+                SA += M0[i][j]**2
         print('SA = ', SA)
         for i in range(len(M0)):
-            Sd=Sd+M0[i][i]
+            Sd += M0[i][i]
         print('Sd = ', Sd)
         for i in range(len(M0)):
             for j in range(len(M0)):
-                if i!=j:
-                    Snd=Snd+M0[i][j]**2
+                if i != j:
+                    Snd += M0[i][j]**2
         print('Snd = ', Snd)
-        main=max_up_diag(M0)
+        main = max_up_diag(M0)
         for k in range(len(M0)):
             for l in range(len(M0)):
-                if k>l:
-                    if M0[k][l]==main:
+                if k > l:
+                    if M0[k][l] == main:
                         i, j = k, l
                         break
-        nu=2*main/(M0[i][i]-M0[j][j])
-        H=np.eye(len(M0))
-        c=np.sqrt(0.5*(1+1/np.sqrt(1+nu**2)))
-        s=np.sign(nu)*np.sqrt(0.5*(1-1/np.sqrt(1+nu**2)))
-        H[i][i]=H[j][j]=c
-        H[i][j]=-s
-        H[j][i]=s
+        nu = 2*main/(M0[i][i] - M0[j][j])
+        H = np.eye(len(M0))
+        c = np.sqrt(0.5*(1 + 1/np.sqrt(1 + nu**2)))
+        s = np.sign(nu)*np.sqrt(0.5*(1 - 1/np.sqrt(1 + nu**2)))
+        H[i][i] = H[j][j] = c
+        H[i][j] = -s
+        H[j][i] = s
         print(H, ' = T')
-        v=np.dot(v, H)
-        M1=((np.transpose(H)).dot(M0)).dot(H)
-        main=max_up_diag(M1)
-        M0=M1
+        v = np.dot(v, H)
+        M1 = ((np.transpose(H)).dot(M0)).dot(H)
+        main = max_up_diag(M1)
+        M0 = M1
     print('Власні числа:')
     for i in range(len(M0)):
         print(f'λ_{i} = ', M0[i][i])
     print('Власні вектори:')
     for i in range(len(v)):
-        print(np.transpose(vectors(v, i)), f' = v_{i+1}')
+        print(np.transpose(vectors(v, i)), f' = v_{i + 1}')
 
 
 Jakobi(A)
-print('By python: \n', np.linalg.eigh(A, UPLO='U'))
+print('By python: \n', np.linalg.eigh(A, UPLO = 'U'))
